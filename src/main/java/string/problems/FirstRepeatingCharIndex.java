a622921d5fcfee08b40f3e9bc579ae25bba6d14b
Rahul Varma <rahul_varma@epam.com>
2024-02-06 11:37:12 +0530

    adding new coding problems

---
+++ src/main/java/string/problems/FirstRepeatingCharIndex.java
@@ -0,0 +1,33 @@
+package string.problems;
+
+public class FirstRepeatingCharIndex {
+
+    public static void main(String[] args) {
+        int ans = findIndexOfFirstRepeatingChar1("loveleetcode");
+        System.out.println(ans);
+    }
+
+    private static int findIndexOfFirstRepeatingChar(String str) {
+
+        for(char ch: str.toCharArray()) {
+            if(str.indexOf(ch)==str.lastIndexOf(ch)){
+                return str.indexOf(ch);
+            }
+        }
+        return -1;
+    }
+
+    private static int findIndexOfFirstRepeatingChar1(String str) {
+
+        int [] arr = new int[26];
+        for(char ch: str.toCharArray()) {
+            arr[ch-'a']++;
+        }
+        for(int i=0;i<str.length();i++) {
+            if(arr[str.charAt(i)-'a']==1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/string/problems/Pangram.java
@@ -0,0 +1,42 @@
+package string.problems;
+
+public class Pangram {
+
+    public static void main(String[] args) {
+        String s = "The quick brown fox jumps over the lazy dog";
+       boolean ans = isPangram1(s);
+        System.out.println(ans);
+    }
+
+    private static boolean isPangram(String s) {
+        if(s.replaceAll("\\s+", "").length()<26){
+            return false;
+        }else {
+            for(char i='a';i<='z';i++) {
+                if(s.indexOf(i)<0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPangram1(String s) {
+        int [] arr = new int[26];
+        char [] chars = s.replaceAll("\\s+", "").toLowerCase().toCharArray();
+        if(s.length()<26){
+            return false;
+        }else {
+            for(char ch : chars) {
+                arr[ch-'a']++;
+            }
+
+            for(int a : arr) {
+                if(a==0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
